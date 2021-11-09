bae039c9cd7c0d5c48ac94e38774daf5a2b84e73
pratiwiade <60900119003@uin-alauddin.ac.id>
2021-11-09 11:58:05 +0800

    Semoga Berhasil

---
+++ resources/views/Version control system.blade.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    Version control system (VCS) adalah sebuah kumpulan perangkat lunak yang sudah terintegrasi dan digunakan untuk membantu software engineer mengelola perubahan dalam source code dari waktu ke waktu
+    Macam-macam version control
+    1. Bazzar
+    2. Subversion (SVN)
+    3. Mercurial
+    4. CVS
+    5. RCS
+</body>
+</html>
